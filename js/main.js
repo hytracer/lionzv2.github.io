400fe1267f8c68f6753f8b8fe4dc2ba565378db4
Lionz <96448965+lionzv2@users.noreply.github.com>
2022-04-16 22:14:18 +0200

    Lowkey true

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //typed settings
 var typed = new Typed('.typed', {
-    strings: ["&nbsp;PP Farmer", "&nbsp;Frikandelbroodje Lover", "&nbsp;Code Noob", "&nbsp;Mcosu Enjoyer"],
+    strings: ["&nbsp;PP Farmer", "&nbsp;Frikandelbroodje Lover", "&nbsp;Code Noob", "&nbsp;Mcosu Enjoyer", "&nbsp;Fumo Connoisseur"],
     typeSpeed: 40,
     startDelay: 1500,
     backSpeed: 30,
@@ -13,5 +13,5 @@ var keys = '';
 window.addEventListener('keypress', function (e) {
     keys += `${e.charCode}+`;
     console.log(keys);
-    if (keys == '108+105+111+110+122+') window.location.replace('https://www.youtube.com/watch?v=6TvDugvV4uQ');
+    if (keys == '108+105+111+110+122+') window.location.replace('https://i.imgur.com/7CiCPWs.png');
 });
